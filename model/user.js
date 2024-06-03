1541a98cf5fb923c0abdd08586c38e710ae5b7ce
choiyoorim <yulim3839@naver.com>
2024-06-03 11:55:46 +0900

    feat:✨ 유저 닉네임 기반 정보 제공 기능

--- model/user.js
+++ model/user.js
@@ -15,7 +15,6 @@ const checkUser = async(email) => {
     return rows
 }
 
-
 // 유저 검색
 const getOneUser = async(email) => {
   let sql = `SELECT * FROM users WHERE email = ${email}`;
@@ -35,9 +34,23 @@ const getUserInfoByUserID = async(userID) => {
   return rows;
 };
 
+
+const getNicknamedUserList = async(nickname) => {
+  let keyword = '%' + nickname + '%';
+  
+  let sql = `
+      SELECT u.userID AS userId, u.nickname AS nickname
+      FROM users u
+      WHERE u.nickname LIKE ? `;
+
+  let [rows] = await db.query(sql, [keyword]);
+  return rows;
+}
+
 module.exports = {
     postJoin,
     checkUser,
     getOneUser,
-    getUserInfoByUserID
+    getUserInfoByUserID,
+    getNicknamedUserList
 }
--- routes/user/index.js
+++ routes/user/index.js
@@ -3,10 +3,13 @@ const joinPOST = require('../../controllers/user/joinPOST');
 const loginPOST = require('../../controllers/user/loginPOST');
 const logoutGET = require('../../controllers/user/logoutGET');
 const userinfoGET = require('../../controllers/user/userinfoGET');
+const userListGET = require('../../controllers/user/userListGET');
 const router = express.Router();
 
 router.post('/join',joinPOST);
 router.post('/login', loginPOST);
 router.get('/logout', logoutGET);
 router.get('/userInfo/:userID',userinfoGET);
+router.get('/search', userListGET);
+
 module.exports = router;
